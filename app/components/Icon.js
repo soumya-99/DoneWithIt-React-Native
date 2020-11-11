08c93810ee440e01edb9169020882a2b0c07f028
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-11-11 22:32:21 +0530

    reusable icon component

---
+++ app/components/Icon.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const Icon = ({
+  name,
+  size = 40,
+  backgroundColor = "#000000",
+  iconColor = "#ffffff",
+}) => {
+  return (
+    <View
+      style={{
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        backgroundColor, // if the key and it's value are same, remove the value and that give you same like before.
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <MaterialCommunityIcons name={name} color={iconColor} size={size * 0.5} />
+    </View>
+  );
+};
+
+export default Icon;
+
+const styles = StyleSheet.create({});
