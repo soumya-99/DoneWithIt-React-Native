1b349a6053c6db7abba2f14b911107db17633e77
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-11-11 23:14:54 +0530

    ListingsScreen component created and completed

--- App.js
+++ App.js
@@ -14,9 +14,10 @@ import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
   return (
-    <AccountScreen />
+    <ListingsScreen />
   );
 }
