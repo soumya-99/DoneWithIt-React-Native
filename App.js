fe1ea5b83c4659db0adc34db4a8e621d7d70e37f
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-11-16 21:37:27 +0530

    TextInput Completed

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import { Text, TextInput } from "react-native";
 import Screen from "./app/components/Screen";
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
-  console.log(firstName);
+	const [firstName, setFirstName] = useState("");
 
 	return (
 		<Screen>
-    <Text>{firstName}</Text>
+			<Text>{firstName}</Text>
 			<TextInput
-				onChange={(e) => setFirstName(e.target.value)}
+				maxLength={5}
+				keyboardType="numeric"
+				secureTextEntry
+				onChangeText={(text) => setFirstName(text)}
 				placeholder="First Name"
 				style={{
 					borderBottomColor: "#ccc",
