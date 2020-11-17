0b16b81b84a1e22f8642fd232c340af0e15e926e
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-11-17 22:32:53 +0530

    ErrorMessage component created for perfectly maintain our applciation

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -7,6 +7,7 @@ import AppTextInput from "../components/AppTextInput";
 import Screen from "../components/Screen";
 import AppButton from "../components/AppButton";
 import AppText from "../components/AppText";
+import ErrorMessage from "../components/ErrorMessage";
 
 const validationSchema = Yup.object().shape({
 	email: Yup.string().required().email().label("Email"),
@@ -33,7 +34,7 @@ const LoginScreen = () => {
 							textContentType="emailAddress"
 							onChangeText={handleChange("email")}
 						/>
-						<AppText style={{ color: "red" }}>{errors.email}</AppText>
+						<ErrorMessage error={errors.email} />
 						<AppTextInput
 							autoCapitalize="none"
 							placeholder="Password"
@@ -43,7 +44,7 @@ const LoginScreen = () => {
 							secureTextEntry
 							onChangeText={handleChange("password")}
 						/>
-                        <AppText style={{ color: "red" }}>{errors.password}</AppText>
+                        <ErrorMessage error={errors.password} />
 						<AppButton title="Login" onPress={handleSubmit} />
 					</>
 				)}
