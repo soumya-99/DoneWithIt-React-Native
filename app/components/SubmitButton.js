e0c0dbef3249837ba1d46ca92eb7941bd1f27635
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-11-25 21:32:54 +0530

    Added SubmitButton Component for cleaner code

---
+++ app/components/SubmitButton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { useFormikContext } from "formik";
+
+import AppButton from './AppButton'
+
+const SubmitButton = ({ title }) => {
+    const {handleSubmit} = useFormikContext();
+    return (
+        <AppButton title={title} onPress={handleSubmit} />
+    )
+}
+
+export default SubmitButton
+
+const styles = StyleSheet.create({})
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -9,6 +9,7 @@ import AppButton from "../components/AppButton";
 import AppText from "../components/AppText";
 import ErrorMessage from "../components/ErrorMessage";
 import AppFormField from "../components/AppFormField";
+import SubmitButton from "../components/SubmitButton";
 
 const validationSchema = Yup.object().shape({
 	email: Yup.string().required().email().label("Email"),
@@ -24,7 +25,7 @@ const LoginScreen = () => {
 				onSubmit={(values) => console.log(values)}
 				validationSchema={validationSchema}
 			>
-				{({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
+				{() => (
 					<>
 						<AppFormField
 							placeholder="Email"
@@ -44,7 +45,7 @@ const LoginScreen = () => {
 							secureTextEntry
 							name="password"
 						/>
-						<AppButton title="Login" onPress={handleSubmit} />
+						<SubmitButton title="Login" />
 					</>
 				)}
 			</Formik>
