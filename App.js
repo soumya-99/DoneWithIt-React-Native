ed7356fb0845d194a82280b25b2865174f009358
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-12-05 21:09:21 +0530

    AppFormPicker component was in wrong folder... Moved to 'forms' folder

--- App.js
+++ App.js
@@ -5,13 +5,14 @@ import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 import AppPicker from "./app/components/AppPicker";
 import LoginScreen from "./app/screens/LoginScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 
 export default function App() {
 
 	return (
 		<Screen>
-			<LoginScreen />
+			<ListingEditScreen />
 		</Screen>
 	);
 }
