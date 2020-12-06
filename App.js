fd3670429ba522e4b591d61a6133cfce47a04ded
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-12-06 20:42:05 +0530

    Added Chevrons in ListItem.js component

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import Screen from "./app/components/Screen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
 
 	return (
 		<Screen>
-			<WelcomeScreen />
+			{/* <WelcomeScreen /> */}
+			<MessagesScreen />
 		</Screen>
 	);
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,61 +1,60 @@
 import React from "react";
-import {
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
 import Swipeable from "react-native-gesture-handler/Swipeable";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import colors from "../config/colors";
 import AppText from "./AppText";
 
 const ListItem = ({
-  title,
-  image,
-  subTitle,
-  onPress,
-  renderRightActions,
-  IconComponent,
+	title,
+	image,
+	subTitle,
+	onPress,
+	renderRightActions,
+	IconComponent,
 }) => {
-  return (
-    <Swipeable renderRightActions={renderRightActions}>
-      <TouchableOpacity onPress={onPress}>
-        <View style={styles.container}>
-          {IconComponent}
-          {image && <Image style={styles.image} source={image} />}
-          <View style={styles.detailsContainer}>
-            <AppText style={styles.title}>{title}</AppText>
-            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
-          </View>
-        </View>
-      </TouchableOpacity>
-    </Swipeable>
-  );
+	return (
+		<Swipeable renderRightActions={renderRightActions}>
+			<TouchableOpacity onPress={onPress}>
+				<View style={styles.container}>
+					{IconComponent}
+					{image && <Image style={styles.image} source={image} />}
+					<View style={styles.detailsContainer}>
+						<AppText style={styles.title}>{title}</AppText>
+						{subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+					</View>
+          <MaterialCommunityIcons color={colors.mediumGrey} name="chevron-right" size={25} />
+				</View>
+			</TouchableOpacity>
+		</Swipeable>
+	);
 };
 
 export default ListItem;
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    padding: 15,
+	container: {
+		flexDirection: "row",
+		padding: 15,
     backgroundColor: colors.white,
-  },
-  image: {
-    width: 70,
-    height: 70,
-    borderRadius: 35,
-    marginRight: 10,
-  },
-  title: {
-    fontWeight: "bold",
-  },
-  subTitle: {
-    color: colors.mediumGrey,
-  },
-  detailsContainer: {
-    marginLeft: 10,
+    alignItems: "center"
+	},
+	image: {
+		width: 70,
+		height: 70,
+		borderRadius: 35,
+		marginRight: 10,
+	},
+	title: {
+		fontWeight: "bold",
+	},
+	subTitle: {
+		color: colors.mediumGrey,
+	},
+	detailsContainer: {
+		marginLeft: 10,
     justifyContent: "center",
-  },
+    flex: 1,
+	},
 });
