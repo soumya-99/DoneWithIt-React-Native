0d9be496bd10f1ecfbe875411f6672cdc93bc2ce
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-12-07 22:27:12 +0530

    Accessing the image library and show selected image on screen

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
 
 import Screen from "./app/components/Screen";
+import { Button, Image } from "react-native";
 
 export default function App() {
+	const [imageUri, setImageUri] = useState();
 	const requestPermission = async () => {
-		const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION);
-		result.granted()
-
-
-		const {granted} = await ImagePicker.requestCameraRollPermissionsAsync();
-		if(!granted) {
-			alert("You need to enable access")
+		const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+		if (!granted) {
+			alert("You need to enable access");
 		}
 	};
 
@@ -20,5 +17,21 @@ export default function App() {
 		requestPermission();
 	}, []);
 
-	return <Screen></Screen>;
+	const selectImage = async () => {
+		try {
+			const result = await ImagePicker.launchImageLibraryAsync();
+			if (!result.cancelled) {
+				setImageUri(result.uri);
+			}
+		} catch (error) {
+			console.log("Error reading an image");
+		}
+	};
+
+	return (
+		<Screen>
+			<Button title="Select Image" onPress={selectImage} />
+			<Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+		</Screen>
+	);
 }
