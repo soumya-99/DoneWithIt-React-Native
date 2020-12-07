909ec9e3d51a5005231b5a0378db6cd7cebd9167
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-12-07 21:44:13 +0530

    Permission Module

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React, { useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
-// import * as Permissions from "expo-permissions";
+import * as Permissions from "expo-permissions";
 
 import Screen from "./app/components/Screen";
 
 export default function App() {
 	const requestPermission = async () => {
-		const result = await ImagePicker.requestCameraRollPermissionsAsync();
-		if(!result.granted) {
+		const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION);
+		result.granted()
+
+
+		const {granted} = await ImagePicker.requestCameraRollPermissionsAsync();
+		if(!granted) {
 			alert("You need to enable access")
 		}
 	};
