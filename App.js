b3c3b0133fad9e115b67ef7654697ccd6251750a
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-12-07 21:41:29 +0530

    Requesting Permissions

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
+// import * as Permissions from "expo-permissions";
 
 import Screen from "./app/components/Screen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
+	const requestPermission = async () => {
+		const result = await ImagePicker.requestCameraRollPermissionsAsync();
+		if(!result.granted) {
+			alert("You need to enable access")
+		}
+	};
 
-	return (
-		<Screen>
-			<ListingEditScreen />
-		</Screen>
-	);
+	useEffect(() => {
+		requestPermission();
+	}, []);
+
+	return <Screen></Screen>;
 }
