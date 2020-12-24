d1939ca06c25d645f2b90e2e46ccbf8e306cebec
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-12-25 00:18:17 +0530

    [2.3.3-un_stb] Stack Navigation Implemented

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
+import { Text } from "react-native";
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
-import ImageInputList from "./app/components/ImageInputList";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+
+const Tweets = () => {
+	<Screen>
+		<Text>Tweets</Text>
+	</Screen>;
+};
+
+const TweetDetails = () => {
+	<Screen>
+		<Text>Tweet Details</Text>
+	</Screen>
+}
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+	<Stack.Navigator>
+		<Stack.Screen name="Tweets" component={Tweets} />
+		<Stack.Screen name="TweetDetails" component={TweetDetails} />
+	</Stack.Navigator>
+)
 
 export default function App() {
 	return (
-		<Screen>
-			<ListingEditScreen />
-		</Screen>
+		<NavigationContainer>
+			<StackNavigator />
+		</NavigationContainer>
 	);
 }
