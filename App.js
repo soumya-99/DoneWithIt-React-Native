d42c7ea74b6b0e8821ec3ad121a667a805bd1bfe
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-12-31 22:45:36 +0530

    [2.3.5] Passing Params to the Routes

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React from "react";
-import { Text } from "react-native";
-import {createStackNavigator} from "@react-navigation/stack";
-import {NavigationContainer} from "@react-navigation/native";
+import { Button, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
 
-const Tweets = () => (
+const Link = () => {
+	const navigation = useNavigation();
+	return (
+		<Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
+	);
+};
+
+const Tweets = ({ navigation }) => (
 	<Screen>
 		<Text>Tweets</Text>
+		<Button
+			title="View Tweets"
+			onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+		/>
 	</Screen>
-)
+);
 
-const TweetDetails = () => (
+const TweetDetails = ({ route }) => (
 	<Screen>
-		<Text>Tweet Details</Text>
+		<Text>Tweet Details {route.params.id}</Text>
 	</Screen>
-)
+);
 
 const Stack = createStackNavigator();
 const StackNavigator = () => (
@@ -23,7 +34,7 @@ const StackNavigator = () => (
 		<Stack.Screen name="Tweets" component={Tweets} />
 		<Stack.Screen name="TweetDetails" component={TweetDetails} />
 	</Stack.Navigator>
-)
+);
 
 export default function App() {
 	return (
