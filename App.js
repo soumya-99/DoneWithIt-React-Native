e79022eaed565f9e3e097127ba42b2951b5134ed
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-12-31 22:48:30 +0530

    [2.3.6] Setting Title Dynamically

--- App.js
+++ App.js
@@ -32,7 +32,11 @@ const Stack = createStackNavigator();
 const StackNavigator = () => (
 	<Stack.Navigator>
 		<Stack.Screen name="Tweets" component={Tweets} />
-		<Stack.Screen name="TweetDetails" component={TweetDetails} />
+		<Stack.Screen
+			name="TweetDetails"
+			component={TweetDetails}
+			options={({route}) => ({ title: route.params.id })}
+		/>
 	</Stack.Navigator>
 );
 
